a13a1d3ef02af88b616da3416953c0c76c546fb7
zxluxscreator <zxluxs851@gmail.com>
2026-07-01 20:38:49 -0300

    dados.py

---
+++ dados.py
@@ -0,0 +1,37 @@
+# ==========================================
+# PROJECT PHOENIX
+# dados.py
+# ==========================================
+
+# -----------------------------
+# DADOS DO JOGADOR
+# -----------------------------
+
+jogador = {
+    "nome": "Luxs",
+    "nivel": 1,
+    "chamas": 0,
+    "ouro": 0,
+
+    "status": {
+        "Saúde": 100,
+        "Energia": 100,
+        "Disciplina": 1,
+        "Aparência": 1,
+        "Corpo": 1,
+        "Estudos": 1,
+        "Touchline": 1,
+        "Programação": 1
+    },
+
+    "missoes": {
+
+        "Tomar Sol": False,
+        "Cardio": False,
+        "Água": False,
+        "Calistenia": False,
+        "Glow Up": False,
+        "Estudos": False
+
+    }
+}
